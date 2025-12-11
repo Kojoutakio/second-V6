7e5609aa7a75f21621a8b2883ee5af99a6d56472
Kojouta <earthkun03@gmail.com>
2025-12-11 23:59:14 +0700

    Update localCertificates.js

--- src/localCertificates.js
+++ src/localCertificates.js
@@ -5,11 +5,11 @@ export const localCertificates = [
     Img: '/serti.jpg',
     Title: '3D Modeling Certificate',
     Issuer: 'Blender Community',
-    Date: '2024',
+    Date: '2025',
     Description: 'Certificate for 3D modeling and character design'
   },
   {
-    id: 'cert',
+    id: 'cert-2',
     Img: '/serti.jpg',
     Title: 'Isometric Design Certificate',
     Issuer: 'Design Academy',
