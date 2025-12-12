dd358c480d8a3a13c8ed019c85083d1713c24062
Kojouta <earthkun03@gmail.com>
2025-12-12 21:17:36 +0700

    Update Home.jsx

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -217,7 +217,7 @@ const Home = () => {
                   isHovering ? "scale-105" : "scale-100"
                 }`}>
                   <img 
-                    src="/Coding.gif" // Path to your GIF in the /public folder
+                    src="/meme-cat.gif" // Path to your GIF in the /public folder
                     alt="3D Modeling and Coding Illustration" 
                     className={gifClassName}
                   />
