3d35381c859e48918d3aa1c8cc8b11591118c110
Kojouta <earthkun03@gmail.com>
2025-12-12 20:13:46 +0700

    Update localCertificates.js

--- src/localCertificates.js
+++ src/localCertificates.js
@@ -9,11 +9,6 @@ export const localCertificates = [
     Description: 'Certificate for 3D modeling and character design'
   },
   {
-    id: 'cert-2',
-    Img: '/serti.jpg',
-    Title: 'Isometric Design Certificate',
-    Issuer: 'Design Academy',
-    Date: '2024',
-    Description: 'Certificate for isometric architectural visualization'
+ 
   }
 ];
